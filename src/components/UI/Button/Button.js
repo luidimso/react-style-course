a1312d47d084bdaacd2e4f686607c02c4cee3765
luidimso <luidi_mso@outlook.com>
2021-08-17 18:12:17 -0300

    Using media query for dynamically style

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -8,6 +8,7 @@ const Button = styled.button`
   background: #8b005d;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
+  width: 100%;
 
   &:focus {
     outline: none;
@@ -18,6 +19,10 @@ const Button = styled.button`
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+
+  @media (min-width: 768px) {
+    width: auto;
   }
 `;
 
